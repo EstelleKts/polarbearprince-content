0fe300891b73fb02ce12c9b78b314ab5da2c899a
Estelle NOEL <estelle.noel@gadz.org>
2020-08-29 11:22:21 +0200

    Updates

---
+++ _next/static/qsOSNiGUBWdZOaVt0_y93/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-618942de299a21bd8b48.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ff3673b4e910f9435a1e.js"],"/about":["static\u002Fchunks\u002Fpages\u002Fabout-a79791c7d34784db3a4d.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
